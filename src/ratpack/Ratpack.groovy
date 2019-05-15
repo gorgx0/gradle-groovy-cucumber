3adc5c72f8d36abfaa47f027532ea1eef069469d
Marcin Krasowski <marcin.krasowski@gmail.com>
2019-05-16 01:44:18 +0200

    handling static assests

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,5 +1,19 @@
+import java.nio.file.Paths
+
 import static ratpack.groovy.Groovy.ratpack
+
 
 ratpack {
 
+
+    serverConfig {
+        c -> c.baseDir(Paths.get('/home/gorg/tmp/ratpack04/app01/src/ratpack'))
+    }
+
+
+    handlers {
+        files {
+            dir("static")
+        }
+    }
 }
