a193c4939ae65f23594b8e85a0a56a715533cc9c
Marcin Krasowski <marcin.krasowski@gmail.com>
2019-05-17 03:30:48 +0200

    modified stepdefs for username

--- src/test/groovy/com/eater/Registrations_Stepdefs.groovy
+++ src/test/groovy/com/eater/Registrations_Stepdefs.groovy
@@ -1,9 +1,18 @@
 package com.eater
 
 import com.eater.test.client.User
+import org.hamcrest.core.IsNull
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.Matchers.equalTo
+import static org.hamcrest.Matchers.notNullValue
 
 this.metaClass.mixin(cucumber.api.groovy.Hooks)
 this.metaClass.mixin(cucumber.api.groovy.EN)
+
+final Logger LOGGER = LoggerFactory.getLogger("Registraion_Sterdefs")
 
 User user
 
@@ -22,13 +31,18 @@ Then(/user is redirected to login\\/register page/) {  ->
     user.redirectLocation == 'registration'
 }
 
+When(/user registers with name {string}/) { String userName ->
+    user.invokeMethod('user registers with name', userName)
+}
+
 When(/user registers/) {  ->
     user.invokeMethod('user opens','/registration')
 
 }
 
 Then(/user is registered user/) {  ->
-    user.userIdCookieValue != null
+    assertThat("Registraion response code", user.statusCode, equalTo(200))
+    assertThat("User id cookie not present or value is empty",user.userIdCookieValue, notNullValue())
 }
 
 World {
